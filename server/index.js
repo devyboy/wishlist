46132f5de6b4ad580c92af723e89ccc1fad614b6
Mike <mhwilkes@gmail.com>
2021-02-04 14:27:47 -0500

    fetch & display based on route param

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import bodyParser from 'body-parser'
+import fetch from 'node-fetch'
 
 const PORT = process.env.PORT || 8081
 
@@ -14,8 +15,13 @@ app.get('/', (_req, res) => {
   res.send('🎉 Hello Wishlist! 🎉');
 });
 
-app.get('/resolver/:txt(.?*)',  (req, res) => {
-  res.send("URL is " + req.params.txt)
+app.get('/resolver/:txt(.?*)',  (req, _res) => {
+  fetch(req.params.txt).then((res) =>
+    res
+    .text()
+    .then((html) => _res.send(html))
+    .catch((err) => console.log(err))
+  )
 })
 
 app.listen(PORT, () =>
